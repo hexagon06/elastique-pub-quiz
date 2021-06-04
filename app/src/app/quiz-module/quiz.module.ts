dbb488e99351128c444546f5c74f05d99d0ccd19
hexagon06 <jasper.saaltink@gmail.com>
2021-06-04 12:19:08 +0200

    quiz-module; quiz-start; generated

---
+++ app/src/app/quiz-module/quiz.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuizStartComponent } from './quiz-start/quiz-start.component';
+
+@NgModule({
+  declarations: [QuizStartComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class QuizModule { }
